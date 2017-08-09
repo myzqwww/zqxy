6b87a3be7f408e75eb3d11dd51ad865769c661ab
2782849498@qq.com <2782849498@qq.com>
2017-08-09 18:53:28 +0800

    响应式页面

---
+++ reactive/php.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+	$('.navbox .fenlei').hover(function(){
+//	alert(123); 
+		$('.erji').show().animate({'height':'400px'},200);
+			 
+		},function(){
+		$('.erji').show().animate({'height':'0px'},200);
+	})	
+})
